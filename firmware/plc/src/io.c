e7cfda1c042fdf068d46b7ad3395292eec2dea83
Miroslav Prymek <prymek@gosw.cz>
2019-10-29 21:06:00 +0100

    `DIS_NUM` etc. defines are not needed anymore

--- firmware/plc/src/io.c
+++ firmware/plc/src/io.c
@@ -2,10 +2,16 @@
 #include "hal.h"
 #include "io.h"
 
-static const uint8_t DI_PIN[DIS_NUM] = DIS_PINS;
-static const uint8_t DO_PIN[DOS_NUM] = DOS_PINS;
-static const uint8_t AI_PIN[AIS_NUM] = AIS_PINS;
-static const uint8_t AO_PIN[AOS_NUM] = AOS_PINS;
+
+static const uint8_t DI_PIN[] = DIS_PINS;
+static const uint8_t DO_PIN[] = DOS_PINS;
+static const uint8_t AI_PIN[] = AIS_PINS;
+static const uint8_t AO_PIN[] = AOS_PINS;
+
+#define DIS_NUM (sizeof(DI_PIN) / sizeof(DI_PIN[0]))
+#define DOS_NUM (sizeof(DO_PIN) / sizeof(DO_PIN[0]))
+#define AIS_NUM (sizeof(AI_PIN) / sizeof(AI_PIN[0]))
+#define AOS_NUM (sizeof(AO_PIN) / sizeof(AO_PIN[0]))
 
 #ifdef DOS_PINS_INVERTED
 #define DO_PIN_VALUE(x) (!(x))
@@ -60,11 +66,11 @@ int io_init()
 
 uint8_t io_set_do(uint8_t index, bool value)
 {
-    log_debug("DO%d (pin %d) := %d", index, DO_PIN[index], value);
     if (index >= DOS_NUM)
     {
         return IO_DOES_NOT_EXIST;
     }
+    log_debug("DO%d (pin %d) := %d", index, DO_PIN[index], value);
     if (set_do_pin_value(DO_PIN[index], DO_PIN_VALUE(value)))
     {
         return IO_HW_ERROR;
@@ -74,11 +80,11 @@ uint8_t io_set_do(uint8_t index, bool value)
 
 uint8_t io_set_ao(uint8_t index, uint16_t value)
 {
-    log_debug("AO%d (pin %d) = %u", index, AO_PIN[index], value);
     if (index >= AOS_NUM)
     {
         return IO_DOES_NOT_EXIST;
     }
+    log_debug("AO%d (pin %d) = %u", index, AO_PIN[index], value);
     if (set_ao_pin_value(AO_PIN[index], AO_PIN_VALUE(value)))
     {
         return IO_HW_ERROR;
